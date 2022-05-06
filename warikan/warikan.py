d91466e1438499e65b95ef422f61b4e0db1b9e9c
yuji96 <y.yama683@gmail.com>
2022-05-06 17:32:24 +0900

    関数化した

--- warikan/warikan.py
+++ warikan/warikan.py
@@ -1,8 +1,10 @@
 import json
 import math
-from typing import NamedTuple
+from typing import List, NamedTuple
 
 import pandas as pd
+
+DF = pd.DataFrame
 
 
 class Row(NamedTuple):
@@ -17,47 +19,51 @@ def round(x, unit=100):
     return int((x + 0.5 * offset) // offset * offset)
 
 
-# members = list("ABCDE")
-# balance = pd.Series([-2000, -1500, 1000, 1000, 1500], index=members)
+def calc_balance(members: List[str]):
+    transition_df = pd.DataFrame(data=0, index=members, columns=members)
+    for row in log_df.itertuples():
+        row: Row
+        if pd.isna(row.note):
+            transition_df.loc[row.payer] += row.amount / len(members)
+        else:
+            transition_df.loc[row.payer] += pd.Series(json.loads(row.note))
 
-log_df = pd.read_csv("data.csv")
+    borrowing = transition_df.sum(axis="rows")
+    lending = transition_df.sum(axis="columns")
+    balance = borrowing - lending
+    return balance
 
-members = list("ABCD")
-transition_df = pd.DataFrame(data=0, index=members, columns=members)
-account_df = transition_df.copy()
-for row in log_df.itertuples():
-    row: Row
-    if pd.isna(row.note):
-        transition_df.loc[row.payer] += row.amount / len(members)
-    else:
-        transition_df.loc[row.payer] += pd.Series(json.loads(row.note))
 
-borrowing = transition_df.sum(axis="rows")
-lending = transition_df.sum(axis="columns")
-balance = borrowing - lending
-init_balance = balance.copy()
+def account(initial_balance: DF, pay_unit=100):
+    balance = initial_balance.copy()
+    account_df = pd.DataFrame(data=0, index=members, columns=members)
 
-pay_unit = 500
-last_eps = 250
-while True:
-    highest_member = balance.idxmax()
-    lowest_member = balance.idxmin()
-    lowest_amount = balance[lowest_member]
-    highest_amount = balance[highest_member]
+    while True:
+        highest_member = balance.idxmax()
+        lowest_member = balance.idxmin()
+        lowest_amount = balance[lowest_member]
+        highest_amount = balance[highest_member]
 
-    if not (lowest_amount < 0 < highest_amount):
-        break
-    if -lowest_amount <= last_eps:
-        break
-    pay = min(-lowest_amount, highest_amount)
-    round_pay = round(pay, unit=pay_unit)
-    # FIXME: round(25) = 50 で無限ループ
-    if round_pay == 0:
-        break
+        if not (lowest_amount < 0 < highest_amount):
+            break
 
-    account_df.loc[highest_member, lowest_member] += round_pay
-    balance[highest_member] -= round_pay
-    balance[lowest_member] += round_pay
+        pay = min(-lowest_amount, highest_amount)
+        if pay < pay_unit:
+            break
+        round_pay = round(pay, unit=pay_unit)
 
-print(pd.DataFrame(dict(init=init_balance, result=balance)))
-print(account_df, end="\n\n")
+        account_df.loc[highest_member, lowest_member] += round_pay
+        balance[highest_member] -= round_pay
+        balance[lowest_member] += round_pay
+
+    return account_df, balance
+
+
+if __name__ == "__main__":
+    log_df = pd.read_csv("data.csv")
+    members = list("ABCD")
+
+    initial_balance = calc_balance(list("ABCD"))
+    account_df, balance = account(initial_balance)
+    print(pd.DataFrame(dict(init=initial_balance, result=balance)))
+    print(account_df, end="\n\n")
