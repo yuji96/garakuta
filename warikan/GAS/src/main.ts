40078b4938f90afce8c2525672440f26d3a4f5ce
yuji96 <y.yama683@gmail.com>
2022-05-07 19:25:06 +0900

    ファイルの分割方針を決めた

---
+++ warikan/GAS/src/main.ts
@@ -0,0 +1,8 @@
+import { Sub } from "./sub";
+
+export function main() {
+  const sub = Sub;
+
+  console.log("hello world");
+  console.log(sub.func());
+}
---
+++ warikan/GAS/src/sub.ts
@@ -0,0 +1,5 @@
+export namespace Sub {
+  export function func() {
+    console.log("called.");
+  }
+}
